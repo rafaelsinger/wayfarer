da26684d824b8dda98321b266ab3edbecb4af565
Rafael Singer <rafaelsinger66@gmail.com>
2024-11-24 23:12:11 -0800

    importing country geojson, react globe component, setting up tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Globe from 'react-globe.gl'
+import countriesGeoJson from './data/countries'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
+        polygonsData={countriesGeoJson.features}
+        polygonAltitude={0.01}
+        polygonCapColor={() => 'rgba(200, 200, 200, 0.9)'}
+        polygonSideColor={() => 'rgba(150, 150, 150, 0.8)'}
+        polygonStrokeColor={() => '#111'}
+      />
+    </div>
   )
 }
 
